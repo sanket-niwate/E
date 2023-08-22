063841ccce626a0becd94c32d3adeac74adb0aa9
SANKET <SANKET@LAPTOP-COV4FKGQ>
2023-08-22 20:35:04 +0530

    sample1

---
+++ src/sample/Example.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="hello";
+        String s2="whatsup";
+        System.out.println("string length is:"+s1.length());
+        System.out.println("string length is:"+s2.length());
+	}
+
+}
